29444b6d3587cf698b7f1324ae2fd7fcc24b03cd
Szewczukm <szewczukm@outlook.com>
2015-11-17 15:49:37 -0500

    updated files

--- src/org/usfirst/frc/team219/robot/RobotMap.java
+++ src/org/usfirst/frc/team219/robot/RobotMap.java
@@ -14,14 +14,33 @@ public class RobotMap {
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
 	
-	public static CANTalon motor;
+	/*
+	 *drive train
+	 *delete comments for puppies 
+	 */
+	public final static int MOTOR_PORT/*_FL*/ = 27;
+	//public final static int MOTOR_PORT_FR = /*insert port here*/
+	//public final static int MOTOR_PORT_BL = /*insert port here*/
+	//public final static int MOTOR_PORT_BR = /*insert port here*/
+	public static CANTalon motor/*_FrontLeft*/;
+	//public static CANTalon motor_FrontRight;
+	//public static CANTalon motor_BackLeft;
+	//public static CANTalon motor_BackRight;
+	//public static RobotDrive driveTrain;
     
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
+    // public static int rangefinsderPort = 1;
     // public static int rangefinderModule = 1;
 	
-	public static void init(){
-		motor = new CANTalon(27);
+	public static void init()
+	{
+		motor/*_FrontLeft*/ = new CANTalon(MOTOR_PORT/*FL*/);
+		//motor_FrontRight = new CANTalon(MOTOR_PORT_FR);
+		//motor_BackLeft = new CANTalon(MOTOR_PORT_BL);
+		//motor_BackRight = new CANTalon(MOTOR_PORT_BR);
+		//driveTrain(motor_FrontLeft, motor_BackLeft, motor_FrontRight, motor_BackRight);
+		
 	}
 }
+
