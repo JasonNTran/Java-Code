ce9be603c2fcfc22c948a7bea77c915fece7ab96
Szewczukm <szewczukm@outlook.com>
2015-11-17 19:05:21 -0500

    Updated joystick axes and tank drive?
    Potentially may have created a simple tank drive system by creating a robotDrive which uses values from the tilt of the xbox 360 controller  left and right joysticks.

--- src/org/usfirst/frc/team219/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team219/robot/subsystems/Drive.java
@@ -5,31 +5,62 @@ import org.usfirst.frc.team219.robot.commands.RunMotor;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drive extends Subsystem
 {
-	CANTalon testMotor = RobotMap.motor;
+	CANTalon motorFL = RobotMap.motor_FrontLeft;
+	CANTalon motorBL = RobotMap.motor_BackLeft;
+	CANTalon motorFR = RobotMap.motor_FrontRight;
+	CANTalon motorBR = RobotMap.motor_BackRight;
 	
+	RobotDrive robotDrive;
+	
+	//This command is run once during instantiation in the Robot.java file.  
+	//It defaultly runs the command to run the motors.
+	//It also creates a drivetrain
+	public void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		setDefaultCommand(new RunMotor());
+		robotDrive = RobotMap.driveTrain;
+	}
+	
+	
+	/*
+	 * 
+	 * This is for the getRawAxis for the xbox 360 controller.  for example the code
+	 * double joystickY_Left = joystick.getRawAxis(2); 
+	 * should theoretically return the value of the left joystick Y axis tilt.
+	 * 
+	 * Axis indexes:
+		1 - LeftX
+		2 - LeftY
+		3 - Triggers (Each trigger = 0 to 1, axis value = right - left)
+		4 - RightX
+		5 - RightY
+		6 - DPad Left/Right
+	 */
 	public void controlMotor(Joystick joystick)
 	{
-		double joystickY = joystick.getAxis(Joystick.AxisType.kY);
-		if(Math.abs(joystickY) <= .2)
+		double joystickY_Left = joystick.getRawAxis(2);
+		double joystickY_Right = joystick.getRawAxis(5);
+		if(Math.abs(joystickY_Left) <= .2)
 		{
-			joystickY = 0;
+			joystickY_Left = 0;
 		}
-		testMotor.set(joystickY);
+		if(Math.abs(joystickY_Right)<= .2)
+		{
+			joystickY_Right = 0;
+		}
+		robotDrive.tankDrive(joystickY_Left, joystickY_Right);
+		
 	}
 	
 	public void stop()
 	{
-		testMotor.set(0.0);
+		robotDrive.stopMotor();
 	}
 	
-	@Override
-	public void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		setDefaultCommand(new RunMotor());
-	}
-	
+
 }
